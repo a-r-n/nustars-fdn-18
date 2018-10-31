9ba7e652f062b4459c9c6a6a7fe2d04cfaa215bd
Aaron <aaron@dhcp-10-105-39-15.wireless.northwestern.private>
2018-10-31 10:28:26 -0500

    init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# nustars-fdn-18
---
+++ main/main.ino
@@ -0,0 +1,77 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+#define RF95_FREQ 915.0
+
+
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+ 
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+ 
+  rf95.setTxPower(23, false);
+}
+
+int packetnum = 0;
+
+void loop() {
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+  
+  char radiopacket[20] = "Hello World #      ";
+  itoa(packetnum++, radiopacket+13, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[19] = 0;
+  
+  Serial.println("Sending...");
+  delay(10);
+  rf95.send((uint8_t *)radiopacket, 20);
+ 
+  Serial.println("Waiting for packet to complete..."); 
+  delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+ 
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+}
