23bc8d2eb0d072f5c38fc21540b57c9f9a2d9e51
John Kuzminskas <j.p.kuz41.3x@gmail.com>
2019-03-05 18:12:35 -0600

    new ground station

--- ground_station/read_data_python.py
+++ ground_station/read_data_python.py
@@ -1,33 +1,41 @@
 import matplotlib.pyplot as plt
 import serial
-usb_port = '/dev/cu.usbmodem14301'
+usb_port = 'COM12'
 ser = serial.Serial(usb_port, 9600)
 from time import sleep
-
-counter = 0
-l = ser.readline()
-
-while l != b'BEGIN\r\n':
-	l = ser.readline()
-l = ser.readline()
-
-
-A = [[],[]]
+import re
+from struct import pack, unpack
+tag_length = 4
+PACKET_SIZE = 19
+NUM_DATA = 3
+A = []
+for i in range(NUM_DATA):
+        A.append([])
 I = []
 plt.ion()
 fig1 = plt.figure()
-ax = fig1.add_subplot(111)
-
-while counter < 10:
-	I.append(counter)
-	counter += 1
-	print(l)
-	data = l.split();
-	A[0].append(int(data[1]))
-	A[1].append(int(data[2]))
-	ax.plot(I, A[0])
-	fig1.canvas.draw()
-
-	l = ser.readline()
-	sleep(.1)
+ax1 = fig1.add_subplot(111)
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+counter = 0
+while counter < 1000:
+        # search for tag
+        l = ser.read(size=tag_length)
+        I.append(counter)
+        counter += 1
+        print(l)
+        data = []
+        i = 0
+        while i < NUM_DATA*4:
+                d=ser.read(size=4)
+                data.append(unpack('f', d))
+                i += 4
+        print(data)
+        A[0].append(data[1])
+        A[1].append(data[2])
+        ax1.plot(I, A[0])
+        fig1.canvas.draw()
+        ax2.plot(I, A[1])
+        plt.show()
+        sleep(.1)
 ser.close()
